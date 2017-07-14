8e19646879bfb8811a7a51dcbd7a21641ec249fe
jenx10 <jeanette.abonce@gmail.com>
2017-07-13 19:46:10 -0500

    Makes sense!
    yay

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+console.log ('hi');
+
+window.onload = function(event) {
+
+	var nameInput = document.getElementById('first_name');
+	var nameBtn = document.getElementById('name_btn');
+
+	nameBtn.onclick = function(event) {
+		// console.log(event);
+		console.log(nameInput.value);
+
+	}
+
+	console.log(nameInput, nameBtn);
+
+}
