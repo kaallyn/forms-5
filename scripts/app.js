8657e5604a063cd0e429ee58c943082e7a120dfa
jenx10 <jeanette.abonce@gmail.com>
2017-07-25 15:41:04 -0500

    updated

--- scripts/app.js
+++ scripts/app.js
@@ -15,22 +15,51 @@ window.onload = function(event) {
 	var nameBtn = document.getElementById('name_btn');
 	var firstChoice = document.getElementById('choice-one');
 	var secondChoice = document.getElementById('choice-two');
-	var secondChoice = document.getElementById('choice-two');
-	var secondChoice = document.getElementById('choice-two');
+	var thirdChoice = document.getElementById('choice-three');
+	var fourthChoice = document.getElementById('choice-four');
+	var fifthChoice = document.getElementById('choice-five');
+	var sixthChoice = document.getElementById('choice-six');
+	var seventhChoice = document.getElementById('choice-seven');
+	var eighthChoice = document.getElementById('choice-eight');
 
 	firstChoice.onclick = function(event) {
 		createRow(firstSentence);
 		document.getElementById('first-step').style.display = 'none';
 	}
 
-	secondChoice.oneclick = function(event) {
+	secondChoice.onclick = function(event) {
 		createRow(secondSentence);
 		document.getElementById('first-step').style.display = 'none';
 	}
 
 	thirdChoice.onclick = function(event) {
 		createRow(thirdSentence);
-		document.getElementById('first-step').style.display = 'none';
+		document.getElementById('second-step').style.display = 'none';
+	}
+
+	fourthChoice.onclick = function(event) {
+		createRow(fourthSentence);
+		document.getElementById('second-step').style.display = 'none';
+	}
+
+	fifthChoice.onclick = function(event) {
+		createRow(fifthSentence);
+		document.getElementById('third-step').style.display = 'none';
+	}
+
+	sixthChoice.onclick = function(event) {
+		createRow(sixthSentence);
+		document.getElementById('third-step').style.display = 'none';
+	}
+
+	seventhChoice.onclick = function(event) {
+		createRow(seventhSentence);
+		document.getElementById('fourth-step').style.display = 'none';
+	}
+
+	eighthChoice.onclick = function(event) {
+		createRow(eighthSentence);
+		document.getElementById('fourth-step').style.display = 'none';
 	}
 
 	nameBtn.onclick = function(event) {
